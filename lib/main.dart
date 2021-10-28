ec0b26c4fe076daee935d8d645ac1ae821ac4c9c
eng_mira <amiram7md231@gmail.com>
2021-10-28 18:16:27 +0200

    install project add empty home Screen

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        MyHomePage.routeName : (BuildContext)=> MyHomePage(),
+
+      },
+      initialRoute: MyHomePage.routeName ,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  static const String routeName ='MyHomePage';
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
